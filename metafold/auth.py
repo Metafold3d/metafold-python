bbc0ac3772df64295780217151be5fcda9e0092d
Rhamel Roomes-Delpeache <rhamel@metafold3d.com>
2024-07-29 15:57:28 -0400

    Add AuthProvider construction to MetafoldClient

--- metafold/auth.py
+++ metafold/auth.py
@@ -1,13 +1,21 @@
 from auth0.authentication import GetToken
 from collections import namedtuple
 from datetime import datetime, timedelta, timezone
+from typing import Optional
 
 Token = namedtuple("Token", ["access_token", "expires_at"])
 
 
 class AuthProvider:
-    def __init__(self, auth_domain: str, client_id: str, client_secret: str):
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        auth_domain: str,
+        base_url: str
+    ) ->  None:
         self._auth_domain = auth_domain
+        self._base_url = base_url
         self._client_id = client_id
 
         self._get_token = GetToken(
@@ -20,7 +28,7 @@ class AuthProvider:
     def get_token(self) -> str:
         now = datetime.now(timezone.utc)
         if not self._token or self._token.expires_at - now < timedelta(minutes=1):
-            token = get_token.client_credentials(self._base_url)
+            token = self._get_token.client_credentials(self._base_url)
             expires_at = now + timedelta(seconds=token["expires_in"])
             self._token = Token(token["access_token"], expires_at)
         return self._token.access_token

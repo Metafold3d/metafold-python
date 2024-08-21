e333b568eaa0575296f32c252d7d9c0fb9d2a761
Rhamel Roomes-Delpeache <rhamel@metafold3d.com>
2024-08-21 15:43:57 -0400

    xform: Add new utility

---
+++ metafold/utils.py
@@ -0,0 +1,30 @@
+from numpy.typing import ArrayLike
+from scipy.spatial.transform import Rotation as R
+from typing import Optional
+import numpy as np
+
+
+def xform(
+    translation: Optional[ArrayLike] = None,
+    rotation: Optional[ArrayLike] = None,
+    scale: Optional[ArrayLike] = None,
+) -> np.array:
+    """Compose transformation matrix.
+
+       Args:
+            translation: Translation in the x, y, and z directions.
+            rotation: Euler angles in degrees. Rotation is applied in the order Rz, Ry, Rx.
+            scale: Scale factor applied in the x, y, z directions before rotation.
+
+        Returns:
+            4 x 4 affine transformation matrix.
+    """
+    translation = translation or np.array([0.0, 0.0, 0.0])
+    rotation = rotation or np.array([0.0, 0.0, 0.0])
+    scale = scale or np.array([1.0, 1.0, 1.0])
+
+    r = R.from_euler("xyz", rotation,  degrees=True).as_matrix()
+    m = np.eye(4)
+    m[:3, :3] = np.diag(scale) @ r
+    m[3, :3] = translation
+    return m
